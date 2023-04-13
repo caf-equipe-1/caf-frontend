540401f080626dedbb8cf1a6b4cdbf52c9dc388b
Felipe Augusto <lipe_augustobq@hotmail.com>
2023-04-12 21:05:06 -0300

    feat: :sparkles: improving login page

--- caf-frontend/src/components/pages/login/index.tsx
+++ caf-frontend/src/components/pages/login/index.tsx
@@ -1,23 +1,33 @@
-import { Link } from "react-router-dom";
-import { ButtonLogin, InputLogin, StyledLoginBox, StyledLoginPage } from "./styles";
+import { useState } from "react";
+import {
+  ButtonConfirm,
+  ButtonRegister,
+  InputLogin,
+  StyledLoginBox,
+  StyledLoginPage,
+} from "./styles";
 
 export function LoginPage() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  function handleModal() {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <>
       <StyledLoginPage>
         <StyledLoginBox>
-        <h2>FAÇA SEU LOGIN</h2>
-        <form>
-          <div>
-            <h3>E-mail</h3>
-            <InputLogin type="text" placeholder="Digite seu e-mail" />
-            <h3>Senha</h3>
-            <InputLogin type="password" placeholder="Digite sua senha" />
-          </div>
-          <ButtonLogin>CONFIRMAR</ButtonLogin>
-          <h4>Ainda não possui login?</h4>
-          {/* <Link to="" >Registre-se</Link> */}
-        </form>
+          <h2>LOGIN</h2>
+          <form>
+            <div>
+              <h3>E-mail</h3>
+              <InputLogin type="text" placeholder="Digite seu e-mail" />
+            </div>
+            <ButtonConfirm>CONFIRMAR</ButtonConfirm>
+            <h4>Ainda não possui login?</h4>
+            <ButtonRegister onClick={handleModal}>Registre-se</ButtonRegister>
+          </form>
         </StyledLoginBox>
       </StyledLoginPage>
     </>
--- caf-frontend/src/components/pages/login/styles.ts
+++ caf-frontend/src/components/pages/login/styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const StyledLoginPage = styled.div`
-  background-color: #00002e;
-  color: #c9c5c9;
+  /* background-color: #00002e;
+  color: #c9c5c9; */
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -29,37 +29,45 @@ export const StyledLoginBox = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 350px;
-  height: 500px;
-  border: 2px solid;
+  width: 400px;
+  height: 450px;
   border-radius: 10px;
+  box-shadow: 0 10px 30px rgba(0, 0, 0, 0.12), 0 10px 20px rgba(0, 0, 0, 0.24);
 `;
 
 export const InputLogin = styled.input`
   padding: 8px;
-  width: 90%;
+  width: 100%;
   margin: 10px;
-  /* border-radius: 5px; */
   border: none;
-  border-bottom: 1px solid #aeaaae;
+  border-bottom: 1px solid;
   font-size: 1.1rem;
   background-color: transparent;
-  color: #c9c5c9;
 `;
 
-export const ButtonLogin = styled.button`
+export const ButtonConfirm = styled.button`
   display: flex;
   margin: 10px auto;
-  padding: 10px;
+  padding: 12px;
   border-radius: 5px;
   border: none;
   cursor: pointer;
   font-weight: 600;
-  background-color: #321ab8;
+  background-color: #000042;
   color: #c9c5c9;
   transition: all 0.2s;
-  
+
   :hover {
     scale: 1.2;
   }
 `;
+
+export const ButtonRegister = styled.button`
+display: flex;
+  margin: 10px auto;
+  padding: 12px;
+  border: none;
+  cursor: pointer;
+  font-weight: 600;
+  background-color: transparent;
+`
