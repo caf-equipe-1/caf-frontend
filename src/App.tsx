861c7e12f725c5e97c79a133e20218840dc3efc8
Nayahto <nayahto157@gmail.com>
2023-04-14 21:12:42 -0300

    feat(App): added React-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./components/forms/Login/login";
 import { Register } from "./components/forms/Register/register";
 
 export function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Login />}></Route>
+          <Route path={"/register"} element={<Register />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
