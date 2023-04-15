3480cdb9cd1ad4f35f30a4f70932a8a7fb48fe7a
Douglas Volcato <douglasvolcato@gmail.com>
2023-04-14 22:38:31 -0300

    feat(infra): implement token store after login response

--- src/infra/api/routers/login/selfieLogin-router.ts
+++ src/infra/api/routers/login/selfieLogin-router.ts
@@ -1,26 +1,37 @@
 import { LoggedUserDto } from "../../../../domain/dtos/login/loggedUser-dto";
 import { SelfieLoginDto } from "../../../../domain/dtos/login/selfieLogin-dto";
+import { HttpResponse } from "../../../../domain/types/http/httpResponse-dto";
 import { ApiConnectionInterface } from "../../../abstract/api/connection/apiConnection-interface";
 import { SelfieLoginRouterInterface } from "../../../abstract/api/routers/login/selfieLogin-router-interface";
 import { HttpRequestAdapterInterface } from "../../../abstract/helpers/adapters/httpRequest-adapter-interface";
+import { TokenStorageInterface } from "../../../abstract/storages/token-storage-interface";
 
 export class SelfieLoginRouter implements SelfieLoginRouterInterface {
   private readonly httpRequest: HttpRequestAdapterInterface;
   private readonly apiConnection: ApiConnectionInterface;
+  private readonly tokenStorage: TokenStorageInterface;
   private readonly route: string;
 
   public constructor(
     httpRequest: HttpRequestAdapterInterface,
-    apiConnection: ApiConnectionInterface
+    apiConnection: ApiConnectionInterface,
+    tokenStorage: TokenStorageInterface
   ) {
     this.httpRequest = httpRequest;
     this.apiConnection = apiConnection;
-    this.route = "login/selfie";
+    this.tokenStorage = tokenStorage;
+    this.route = "login/email";
   }
 
-  public async login(loginInfo: SelfieLoginDto): Promise<LoggedUserDto> {
+  public async login(
+    loginInfo: SelfieLoginDto
+  ): Promise<HttpResponse<LoggedUserDto>> {
     const apiLink = `${this.apiConnection.getLink()}/${this.route}`;
+    const response = await this.httpRequest.post(apiLink, loginInfo);
+    const token = response.body.token;
 
-    return await this.httpRequest.post(apiLink, loginInfo);
+    this.tokenStorage.store(token);
+
+    return response;
   }
 }
