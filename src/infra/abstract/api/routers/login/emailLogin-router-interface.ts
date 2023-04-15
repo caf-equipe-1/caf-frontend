49c95bd6e623c1041cb63bcde0218d293eef613c
Douglas Volcato <douglasvolcato@gmail.com>
2023-04-14 22:37:33 -0300

    fix(infra): change return types

--- src/infra/abstract/api/routers/login/emailLogin-router-interface.ts
+++ src/infra/abstract/api/routers/login/emailLogin-router-interface.ts
@@ -1,6 +1,7 @@
 import { EmailLoginDto } from "../../../../../domain/dtos/login/emailLogin-dto";
 import { LoggedUserDto } from "../../../../../domain/dtos/login/loggedUser-dto";
+import { HttpResponse } from "../../../../../domain/types/http/httpResponse-dto";
 
 export interface EmailLoginRouterInterface {
-  login(loginInfo: EmailLoginDto): Promise<LoggedUserDto>;
+  login(loginInfo: EmailLoginDto): Promise<HttpResponse<LoggedUserDto>>;
 }
--- src/infra/abstract/api/routers/login/selfieLogin-router-interface.ts
+++ src/infra/abstract/api/routers/login/selfieLogin-router-interface.ts
@@ -1,6 +1,7 @@
 import { LoggedUserDto } from "../../../../../domain/dtos/login/loggedUser-dto";
 import { SelfieLoginDto } from "../../../../../domain/dtos/login/selfieLogin-dto";
+import { HttpResponse } from "../../../../../domain/types/http/httpResponse-dto";
 
 export interface SelfieLoginRouterInterface {
-  login(loginInfo: SelfieLoginDto): Promise<LoggedUserDto>;
+  login(loginInfo: SelfieLoginDto): Promise<HttpResponse<LoggedUserDto>>;
 }
