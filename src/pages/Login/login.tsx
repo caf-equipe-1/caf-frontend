34ef21dff573f80e0738f57b86cd7327cc8acd42
Douglas Volcato <douglasvolcato@gmail.com>
2023-04-18 00:23:59 -0300

    feat(pages): implement api connection

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -6,26 +6,57 @@ import {
   StyledLoginBox,
   StyledLoginPage,
 } from "./styles";
+import { useState } from "react";
+import { EmailLoginDto } from "../../domain/dtos/login/emailLogin-dto";
+import { makeEmailLoginRouterFactory } from "../../infra/api/factories/login/emailLogin-router-factory";
 
 export function Login() {
   const navigate = useNavigate();
+  const [loginData, setLoginData] = useState<EmailLoginDto>({
+    email: "",
+    password: "",
+  });
+
+  function handleLogin() {
+    const loginRouter = makeEmailLoginRouterFactory();
+
+    loginRouter.login(loginData);
+  }
 
   return (
     <>
       <StyledLoginPage>
         <StyledLoginBox>
           <h2>LOGIN</h2>
-          <form>
+          <div>
             <div>
               <h3>E-mail</h3>
-              <InputLogin type="text" placeholder="Digite seu e-mail" />
+              <InputLogin
+                onChange={(event) =>
+                  setLoginData({ ...loginData, email: event.target.value })
+                }
+                type="text"
+                placeholder="Digite seu e-mail"
+              />
             </div>
-            <ButtonConfirm>CONFIRMAR</ButtonConfirm>
+            <div>
+              <h3>Senha</h3>
+              <InputLogin
+                onChange={(event) =>
+                  setLoginData({ ...loginData, password: event.target.value })
+                }
+                type="password"
+                placeholder="Digite sua senha"
+              />
+            </div>
+            <ButtonConfirm onClick={() => handleLogin()}>
+              CONFIRMAR
+            </ButtonConfirm>
             <h4>Ainda não possui login?</h4>
             <ButtonRegister onClick={() => navigate("register")}>
               Registre-se
             </ButtonRegister>
-          </form>
+          </div>
         </StyledLoginBox>
       </StyledLoginPage>
     </>
--- src/pages/Register/register.tsx
+++ src/pages/Register/register.tsx
@@ -6,31 +6,78 @@ import {
   StyledRegisterBox,
   StyledRegisterPage,
 } from "./styles";
+import { makeUserRouterFactory } from "../../infra/api/factories/user/user-router-factory";
+import { useState } from "react";
+import { CreateUserDto } from "../../domain/dtos/user/createUser-dto";
 
 export function Register() {
   const navigate = useNavigate();
+  const [userInfo, setUserInfo] = useState<CreateUserDto>({
+    name: "",
+    email: "",
+    cpf: "",
+    password: "",
+    photo: "",
+  });
+
+  function handleRegistration() {
+    const userRegistrationRouter = makeUserRouterFactory();
+
+    userRegistrationRouter.create(userInfo);
+  }
+
   return (
-    <>
-      <StyledRegisterPage>
-        <StyledRegisterBox>
-          <h2>REGISTRO</h2>
-          <form>
-            <div>
-              <h3>E-mail</h3>
-              <InputRegister type="text" placeholder="Digite seu e-mail" />
-            </div>
-            <div>
-              <h3>Senha</h3>
-              <InputRegister type="text" placeholder="Digite sua senha" />
-            </div>
-            <ButtonConfirm>CONFIRMAR</ButtonConfirm>
-            <h4>Já possui login?</h4>
-            <ButtonRegister onClick={() => navigate("/")}>
-              Faça login
-            </ButtonRegister>
-          </form>
-        </StyledRegisterBox>
-      </StyledRegisterPage>
-    </>
+    <StyledRegisterPage>
+      <StyledRegisterBox>
+        <h2>REGISTRO</h2>
+        <form>
+          <div>
+            <h3>Nome</h3>
+            <InputRegister
+              type="text"
+              placeholder="Digite seu nome"
+              onChange={(event) =>
+                setUserInfo({ ...userInfo, name: event.target.value })
+              }
+            />
+          </div>
+          <div>
+            <h3>E-mail</h3>
+            <InputRegister
+              type="text"
+              placeholder="Digite seu e-mail"
+              onChange={(event) =>
+                setUserInfo({ ...userInfo, email: event.target.value })
+              }
+            />
+          </div>
+          <div>
+            <h3>CPF</h3>
+            <InputRegister
+              type="number"
+              placeholder="Digite seu CPF"
+              onChange={(event) =>
+                setUserInfo({ ...userInfo, cpf: event.target.value.toString() })
+              }
+            />
+          </div>
+          <div>
+            <h3>Senha</h3>
+            <InputRegister
+              type="text"
+              placeholder="Digite sua senha"
+              onChange={(event) =>
+                setUserInfo({ ...userInfo, password: event.target.value })
+              }
+            />
+          </div>
+          <ButtonConfirm>CONFIRMAR</ButtonConfirm>
+          <h4>Já possui login?</h4>
+          <ButtonRegister onClick={() => navigate("/")}>
+            Faça login
+          </ButtonRegister>
+        </form>
+      </StyledRegisterBox>
+    </StyledRegisterPage>
   );
 }
