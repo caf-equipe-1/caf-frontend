882c675cfc2c61691dc40608b682c3fa59520e8c
Nayahto <nayahto157@gmail.com>
2023-04-21 15:48:15 -0300

    feat(photo. style): updated button, img

--- src/pages/photos/style.ts
+++ src/pages/photos/style.ts
@@ -13,4 +13,16 @@ export const PhotoDiv = styled.div`
 export const PhotoButton = styled.button`
   width: 200px;
   height: 30px;
+  background-color: #414bb2;
+  color: white;
+  border-radius: 7px;
+  &: hover {
+    cursor: pointer;
+  }
+`;
+export const PhotoImg = styled.img`
+  border-radius: 50%;
+  width: 250px;
+  height: 260px;
+  border: 1px solid black;
 `;
