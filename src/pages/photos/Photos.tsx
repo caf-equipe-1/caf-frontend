77d7369a261a730325ab035b094a3c3ecbfe5b70
Nayahto <nayahto157@gmail.com>
2023-04-21 15:15:21 -0300

    feat(photo): update h2, h3

--- src/pages/photos/Photos.tsx
+++ src/pages/photos/Photos.tsx
@@ -3,7 +3,10 @@ import { PhotoDiv } from "./style";
 export function Photos() {
   return (
     <>
-      <PhotoDiv></PhotoDiv>
+      <PhotoDiv>
+        <h2>Agora é hora da selfie! </h2>
+        <h3>Posicione seu rosto dentro do quadro, sorria e tire a foto.</h3>
+      </PhotoDiv>
     </>
   );
 }
