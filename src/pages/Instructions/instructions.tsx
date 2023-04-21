4cc5086087a57ab72d45fb1495962847b467d54a
Nayahto <nayahto157@gmail.com>
2023-04-21 09:49:14 -0300

    feat(instructions): updated imports, ol, li

--- src/pages/Instructions/instructions.tsx
+++ src/pages/Instructions/instructions.tsx
@@ -1,32 +1,37 @@
-import { InstructionsDiv, instructionsList } from "./styles";
+import {
+  InstructionsDiv,
+  InstructionH2,
+  InstructionH3,
+  Instructionli,
+} from "./styles";
 
 export function Instructions() {
   return (
     <>
       <InstructionsDiv>
-        <h2>Agora é hora da selfie!!</h2>
-        <h3>Siga as orientações abaixo:</h3>
+        <InstructionH2>Agora é hora da selfie!</InstructionH2>
+        <InstructionH3>Orientações logo abaixo:</InstructionH3>
         <ol>
-          <instructionsList>
+          <Instructionli>
             Encontre um lugar bem iluminado: escolha um ambiente com boa
             iluminação natural ou artificial para garantir que sua foto fique
             clara e nítida.
-          </instructionsList>
-          <instructionsList>
+          </Instructionli>
+          <Instructionli>
             Enquadre seu rosto: posicione o rosto no centro do quadro da câmera
             e mantenha-o em uma posição frontal, com a cabeça reta e os olhos
             abertos.
-          </instructionsList>
-          <instructionsList>
+          </Instructionli>
+          <Instructionli>
             Retire óculos e chapéus: se você usa óculos ou chapéu, retire-os
             antes de tirar a foto para garantir que seu rosto esteja totalmente
             visível.
-          </instructionsList>
-          <instructionsList>
+          </Instructionli>
+          <Instructionli>
             Evite sombras no rosto: evite que haja sombras no rosto que possam
             interferir na qualidade da foto. Certifique-se de que sua face
             esteja bem iluminada.
-          </instructionsList>
+          </Instructionli>
         </ol>
       </InstructionsDiv>
     </>
