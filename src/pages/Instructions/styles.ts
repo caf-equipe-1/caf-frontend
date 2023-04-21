e0151acaa6d39471c63d05fa5d1596808bd76abd
Nayahto <nayahto157@gmail.com>
2023-04-21 10:00:49 -0300

    feat(instruction.style): updated div, h3, li

--- src/pages/Instructions/styles.ts
+++ src/pages/Instructions/styles.ts
@@ -1,17 +1,16 @@
 import styled from "styled-components";
 
 export const InstructionsDiv = styled.div`
-  border: 1px solid black;
-  height: 200px;
-  width: 90%;
-  margin-left: 20px;
+  padding: 10px;
 `;
 export const InstructionH2 = styled.h2`
   text-align: center;
 `;
 export const InstructionH3 = styled.h3`
   margin-top: 10px;
-  text-align: center;
 `;
 
-export const Instructionli = styled.li``;
+export const Instructionli = styled.li`
+  margin-top: 10px;
+  margin-left: 20px;
+`;
