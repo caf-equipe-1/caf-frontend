d6d8747d7c63126d3a0c6fdeb17186cfd1a032af
Nayahto <nayahto157@gmail.com>
2023-04-21 09:50:43 -0300

    feat(instrucion.style): updated h2, h3, ol, li

--- src/pages/Instructions/styles.ts
+++ src/pages/Instructions/styles.ts
@@ -6,3 +6,12 @@ export const InstructionsDiv = styled.div`
   width: 90%;
   margin-left: 20px;
 `;
+export const InstructionH2 = styled.h2`
+  text-align: center;
+`;
+export const InstructionH3 = styled.h3`
+  margin-top: 10px;
+  text-align: center;
+`;
+
+export const Instructionli = styled.li``;
