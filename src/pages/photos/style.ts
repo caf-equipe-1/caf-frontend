354b7c1304310a864c59d3728a9cb2e3e9bcb076
Nayahto <nayahto157@gmail.com>
2023-04-21 15:25:35 -0300

    feat(photo.style): updated div

--- src/pages/photos/style.ts
+++ src/pages/photos/style.ts
@@ -5,4 +5,12 @@ export const PhotoDiv = styled.div`
   width: 500px;
   height: 500px;
   border: 1px solid black;
+  display: grid;
+  justify-items: center;
+  text-align: center;
+`;
+
+export const PhotoButton = styled.button`
+  width: 200px;
+  height: 30px;
 `;
