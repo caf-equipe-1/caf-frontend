c4f5377d6b909014d489b8339884b0fde2803ec8
Douglas Volcato <douglasvolcato@gmail.com>
2023-04-20 21:32:24 -0300

    feat(main): add service page imports

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import { Login } from "./pages/Login/login";
 import { Register } from "./pages/Register/register";
 import { Header } from "./components/header";
 import { Footer } from "./components/footer";
-import { Services } from "./pages/services/services";
+import { Services } from "./pages/Services/services";
+import { Documents } from "./pages/Documents/documents";
+import { Cards } from "./pages/Cards/cards";
+import { Passwords } from "./pages/Passwords/passwords";
 
 export function App() {
   return (
@@ -15,6 +18,9 @@ export function App() {
           <Route path={"/"} element={<Login />}></Route>
           <Route path={"/register"} element={<Register />}></Route>
           <Route path={"/services"} element={<Services />}></Route>
+          <Route path={"/documents"} element={<Documents />}></Route>
+          <Route path={"/cards"} element={<Cards />}></Route>
+          <Route path={"/passwords"} element={<Passwords />}></Route>
         </Routes>
         <Footer />
       </BrowserRouter>
