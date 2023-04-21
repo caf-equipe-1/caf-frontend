44354209902036df44c31331cee861b3affa2ee1
Douglas Volcato <douglasvolcato@gmail.com>
2023-04-20 21:32:02 -0300

    feat(components): add header click navigation

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,13 @@
 import { HeaderBody, HeaderTitle, HeaderLogo, HeaderMargin } from "./styled";
 import logo from "../../Img/CAFImg.jpg";
+import { useNavigate } from "react-router-dom";
 
 export function Header() {
+  const navigate = useNavigate();
+
   return (
     <>
-      <HeaderBody>
+      <HeaderBody onClick={() => navigate("/services")}>
         <HeaderLogo src={logo} />
         <HeaderTitle>Header</HeaderTitle>
       </HeaderBody>
