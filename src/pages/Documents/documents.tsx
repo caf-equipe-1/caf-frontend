ddd33a78634b94afb87de0d917665191d4e295bd
Douglas Volcato <douglasvolcato@gmail.com>
2023-04-23 18:34:49 -0300

    feat(pages): implement document crud

--- src/pages/Documents/documents.tsx
+++ src/pages/Documents/documents.tsx
@@ -6,10 +6,25 @@ import { makeDocumentRouterFactory } from "../../infra/api/factories/document/do
 import { Document } from "../../domain/entities/document/document-entity";
 import { Card } from "../../components/card";
 import { ActionsTitle } from "../../components/actionsTitle";
+import { CreateDocumentDto } from "../../domain/dtos/document/createDocument-dto";
+import { UpdateDocumentDto } from "../../domain/dtos/document/updateDocument-dto";
+import { Form } from "../../components/Form";
+import { Modal } from "../../components/modal";
 
 export function Documents() {
   const [documents, setDocuments] = useState<Document[]>([]);
   const documentRouter = makeDocumentRouterFactory();
+  const [openCreationModal, setOpenCreationModal] = useState<boolean>(false);
+  const [openUpdateModal, setOpenUpdateModal] = useState<boolean>(false);
+  const [createdDocument, setCreatedDocument] = useState<CreateDocumentDto>({
+    name: "",
+    document: "",
+  });
+  const [updatedDocument, setUpdatedDocument] = useState<UpdateDocumentDto>({
+    name: "",
+    document: "",
+  });
+  const [documentId, setDocumentId] = useState<string>("");
 
   function getDocumentsFromApi() {
     documentRouter.getAll().then(function (response: HttpResponse<Document[]>) {
@@ -17,24 +32,6 @@ export function Documents() {
         setDocuments(response.body);
       }
     });
-  }
-
-  function deleteDocument(documentId: string) {
-    documentRouter.delete(documentId).then(() => {
-      getDocumentsFromApi();
-    });
-  }
-
-  function editDocument(documentId: string) {
-    alert("Implementar navegação para página de edição.");
-  }
-
-  function createDocument() {
-    alert("Implementar navegação para página de criação.");
-  }
-
-  function downloadDocument(documentId: string) {
-    alert("Implementar download de documentos.");
   }
 
   function renderCards() {
@@ -46,12 +43,147 @@ export function Documents() {
           content={[{ label: "Nome", text: document.name }]}
           key={index}
           deleteCallback={deleteDocument}
-          editCallback={editDocument}
+          editCallback={openUpdateDocumentModal}
           downloadButton={true}
           downloadCallback={downloadDocument}
         />
       );
     });
+  }
+
+  function deleteDocument(documentId: string) {
+    if (window.confirm("Deseja apagar esse documento?")) {
+      documentRouter.delete(documentId).then(() => {
+        getDocumentsFromApi();
+      });
+    }
+  }
+
+  function openCreateDocumentModal() {
+    setCreatedDocument({
+      name: "",
+      document: "",
+    });
+
+    setOpenCreationModal(true);
+  }
+
+  function createDocument() {
+    documentRouter.create(createdDocument).then(function (response) {
+      if (response.error) {
+        alert(response.message);
+      } else {
+        setOpenCreationModal(false);
+        getDocumentsFromApi();
+      }
+    });
+  }
+
+  function onNameChangeCreate(inputName: string) {
+    setCreatedDocument({ ...createdDocument, name: inputName });
+  }
+
+  function onDocumentChangeCreate(inputDocument: string) {
+    setCreatedDocument({ ...createdDocument, document: inputDocument });
+  }
+
+  function createDocumentForm() {
+    return (
+      <Form
+        title="Adicionar Documento"
+        fields={[
+          {
+            label: "Nome",
+            inputType: "text",
+            placeholder: "Nome do documento",
+            onChangeCallback: onNameChangeCreate,
+          },
+          {
+            label: "Arquivo",
+            inputType: "file",
+            placeholder: "",
+            onChangeCallback: onDocumentChangeCreate,
+          },
+        ]}
+        buttons={[
+          {
+            label: "Enviar",
+            onClickCallback: createDocument,
+            color: "white",
+            backGroundColor: "LimeGreen",
+          },
+        ]}
+      />
+    );
+  }
+
+  function openUpdateDocumentModal(documentId: string) {
+    const foundDocument = documents.find(function (item) {
+      return item.id.toString() === documentId;
+    });
+
+    if (foundDocument) {
+      setUpdatedDocument(foundDocument);
+      setDocumentId(documentId);
+    }
+
+    setOpenUpdateModal(true);
+  }
+
+  function updateDocument() {
+    documentRouter
+      .update(documentId, updatedDocument)
+      .then(function (response) {
+        if (response.error) {
+          alert(response.message);
+        } else {
+          setOpenUpdateModal(false);
+          getDocumentsFromApi();
+        }
+      });
+  }
+
+  function onNameChangeUpdate(inputName: string) {
+    setUpdatedDocument({ ...createdDocument, name: inputName });
+  }
+
+  function onDocumentChangeUpdate(inputDocument: string) {
+    setUpdatedDocument({ ...createdDocument, document: inputDocument });
+  }
+
+  function updateDocumentForm() {
+    return (
+      <Form
+        title="Editar Documento"
+        fields={[
+          {
+            label: "Nome",
+            inputType: "text",
+            placeholder: "Nome do documento",
+            defaultValue: updatedDocument.name,
+            onChangeCallback: onNameChangeUpdate,
+          },
+          {
+            label: "Arquivo",
+            inputType: "file",
+            placeholder: "",
+            onChangeCallback: onDocumentChangeUpdate,
+          },
+        ]}
+        buttons={[
+          {
+            label: "Enviar",
+            onClickCallback: updateDocument,
+            color: "white",
+            backGroundColor: "LimeGreen",
+          },
+        ]}
+      />
+    );
+  }
+
+  function downloadDocument(documentId: string) {
+    alert("Implementar download de documentos.");
   }
 
   useEffect(function () {
@@ -61,8 +193,18 @@ export function Documents() {
   return (
     <>
       <Title title="Documentos:" />
-      <ActionsTitle createEntityCallback={createDocument} />
+      <ActionsTitle createEntityCallback={openCreateDocumentModal} />
       <FlexBody components={renderCards()} />
+      <Modal
+        show={openCreationModal}
+        setShowCallback={setOpenCreationModal}
+        content={createDocumentForm()}
+      />
+      <Modal
+        show={openUpdateModal}
+        setShowCallback={setOpenUpdateModal}
+        content={updateDocumentForm()}
+      />
     </>
   );
 }
