f22f0b27002c16366833e7ac1ddde5bae99422b3
Felipe Augusto <lipe_augustobq@hotmail.com>
2023-04-26 22:10:24 -0300

    feat: :sparkles: creating camera modal

--- src/pages/Register/register.tsx
+++ src/pages/Register/register.tsx
@@ -1,12 +1,14 @@
 import { useNavigate } from "react-router";
 import { ButtonRegister } from "./styles";
 import { makeUserRouterFactory } from "../../infra/api/factories/user/user-router-factory";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { CreateUserDto } from "../../domain/dtos/user/createUser-dto";
 import { Form } from "../../components/Form";
+import { Modal } from "../../components/modal";
 
 export function Register() {
   const navigate = useNavigate();
+  const [cameraModal, setCameraModal] = useState<boolean>(false);
   const [userInfo, setUserInfo] = useState<CreateUserDto>({
     name: "",
     email: "",
@@ -48,57 +50,113 @@ export function Register() {
     setUserInfo({ ...userInfo, photo: convertedImage });
   }
 
-  return (
-    <Form
-      title="REGISTRO"
-      fields={[
-        {
-          label: "Nome",
-          inputType: "text",
-          placeholder: "Digite o seu nome",
-          onChangeCallback: onNameChange,
-        },
-        {
-          label: "Email",
-          inputType: "email",
-          placeholder: "Digite o seu email",
-          onChangeCallback: onEmailChange,
-        },
-        {
-          label: "CPF",
-          inputType: "number",
-          placeholder: "Digite o seu CPF",
-          onChangeCallback: onCpfChange,
-        },
-        {
-          label: "Senha",
-          inputType: "password",
-          placeholder: "Digite a sua senha",
-          onChangeCallback: onPasswordChange,
-        },
-        {
-          label: "Foto",
-          inputType: "file",
-          placeholder: "",
-          onChangeCallback: onImageChange,
-        },
-      ]}
-      buttons={[
-        {
-          label: "CONFIRMAR",
-          onClickCallback: handleRegistration,
-          color: "white",
-          backGroundColor: "MidnightBlue",
-        },
-      ]}
-      finalContent={
-        <div>
-          <h4>Já possui login?</h4>
-          <ButtonRegister onClick={() => navigate("/")}>
-            Faça login
-          </ButtonRegister>
-        </div>
+  function openCamera() {
+    setCameraModal(true);
+  }
+
+  function Camera() {
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
+
+    const startCamera = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+          setMediaStream(stream);
+        }
+      } catch (err) {
+        console.error("Error accessing camera:", err);
       }
-    />
+    };
+
+    const stopCamera = () => {
+      if (mediaStream) {
+        mediaStream.getTracks().forEach((track) => track.stop());
+        setMediaStream(null);
+      }
+    };
+
+    return (
+      <div>
+        {mediaStream ? (
+          <div>
+            <video ref={videoRef} autoPlay />
+            <button onClick={stopCamera}>Stop Camera</button>
+          </div>
+        ) : (
+          <button onClick={startCamera}>Start Camera</button>
+        )}
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <Form
+        title="REGISTRO"
+        fields={[
+          {
+            label: "Nome",
+            inputType: "text",
+            placeholder: "Digite o seu nome",
+            onChangeCallback: onNameChange,
+          },
+          {
+            label: "Email",
+            inputType: "email",
+            placeholder: "Digite o seu email",
+            onChangeCallback: onEmailChange,
+          },
+          {
+            label: "CPF",
+            inputType: "number",
+            placeholder: "Digite o seu CPF",
+            onChangeCallback: onCpfChange,
+          },
+          {
+            label: "Senha",
+            inputType: "password",
+            placeholder: "Digite a sua senha",
+            onChangeCallback: onPasswordChange,
+          },
+          // {
+          //   label: "Foto",
+          //   inputType: "file",
+          //   placeholder: "",
+          //   onChangeCallback: onImageChange,
+          // },
+        ]}
+        buttons={[
+          {
+            label: "CADASTRAR FOTO",
+            onClickCallback: openCamera,
+            color: "white",
+            backGroundColor: "Green",
+          },
+          {
+            label: "CONFIRMAR",
+            onClickCallback: handleRegistration,
+            color: "white",
+            backGroundColor: "MidnightBlue",
+          },
+        ]}
+        finalContent={
+          <div>
+            <h4>Já possui login?</h4>
+            <ButtonRegister onClick={() => navigate("/")}>
+              Faça login
+            </ButtonRegister>
+          </div>
+        }
+      />
+      <Modal
+        content={Camera()}
+        show={cameraModal}
+        setShowCallback={setCameraModal}
+      />
+    </>
   );
 }
