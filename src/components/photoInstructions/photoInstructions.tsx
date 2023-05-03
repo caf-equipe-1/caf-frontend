54d7c3eae76a412e2318dd6dc508df3fe2adae9e
Douglas Volcato <douglasvolcato@gmail.com>
2023-05-02 21:08:55 -0300

    feat(pages): implement photo capture instructions

--- src/components/photoInstructions/photoInstructions.tsx
+++ src/components/photoInstructions/photoInstructions.tsx
@@ -1,16 +1,8 @@
-import {
-  InstructionsDiv,
-  InstructionH2,
-  InstructionH3,
-  Instructionli,
-  InstructionButton,
-} from "./styles";
+import { InstructionsDiv, Instructionli } from "./styles";
 
-export function Instructions() {
+export function PhotoInstructions() {
   return (
     <InstructionsDiv>
-      <InstructionH2>Agora é hora da selfie!</InstructionH2>
-      <InstructionH3>Orientações logo abaixo:</InstructionH3>
       <ol>
         <Instructionli>
           Encontre um lugar bem iluminado: escolha um ambiente com boa
@@ -26,13 +18,7 @@ export function Instructions() {
           Retire óculos e chapéus: se você usa óculos ou chapéu, retire-os antes
           de tirar a foto para garantir que seu rosto esteja totalmente visível.
         </Instructionli>
-        <Instructionli>
-          Evite sombras no rosto: evite que haja sombras no rosto que possam
-          interferir na qualidade da foto. Certifique-se de que sua face esteja
-          bem iluminada.
-        </Instructionli>
       </ol>
-      <InstructionButton>prosseguir</InstructionButton>
     </InstructionsDiv>
   );
 }
---
+++ src/components/photoInstructions/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const InstructionsDiv = styled.div`
+  margin-bottom: 20px;
+`;
+
+export const Instructionli = styled.li`
+  margin-top: 10px;
+  margin-left: 20px;
+`;
